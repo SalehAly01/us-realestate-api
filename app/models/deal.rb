6c554ec5cb3e0e5f2bf931b6e1324518db0283f5
Saleh Aly <salehaly01@gmail.com>
2017-09-09 23:07:44 +0200

    Update schema, add validation

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Deal < ApplicationRecord
-  validates :street, presence: true
-  validates :zip, presence: true
-  validates :city, presence: true
-  validates :beds, presence: true, numericality: true
-  validates :baths, presence: true, numericality: true
-  validates :sq_ft, presence: true, numericality: true
-  validates :type, presence: true
-  validates :sale_date, presence: true
-  validates :price, presence: true, numericality: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  validates :street, presence: true, allow_blank: false, allow_nil: false
+  validates :zip, presence: true, allow_blank: false, allow_nil: false
+  validates :city, presence: true, allow_blank: false, allow_nil: false
+  validates :beds, presence: true, numericality: true, allow_blank: false, allow_nil: false
+  validates :baths, presence: true, numericality: true, allow_blank: false, allow_nil: false
+  validates :sq_ft, presence: true, numericality: true, allow_blank: false, allow_nil: false
+  validates :realty_type, presence: true, allow_blank: false, allow_nil: false
+  validates :sale_date, presence: true, allow_blank: false, allow_nil: false
+  validates :price, presence: true, numericality: true, allow_blank: false, allow_nil: false
+  validates :latitude, presence: true, allow_blank: false, allow_nil: false
+  validates :longitude, presence: true, allow_blank: false, allow_nil: false
 end
